9d42c8a933c592252aae8f6bb4d286e354b17751
Taewoong-Kim <hiber_01@naver.com>
2023-11-15 09:55:25 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png">
+    <HelloWorld/> -->
+        <TodoHeader></TodoHeader>
+        <TodoInput></TodoInput>
+        <TodoList></TodoList>
+        <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    "TodoHeader": TodoHeader,
+    "TodoInput": TodoInput,
+    "TodoList": TodoList,
+    "TodoFooter": TodoFooter
+  }
+}
+</script>
+
+<style>
+body {
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
